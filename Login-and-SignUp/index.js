5400636695a7f2b9b71a53b5eadadf74bfee26b0
ElshadSadigov <Sv.Elshad@gmail.com>
2023-01-01 11:33:29 +0400

    HTML, CSS and JS

---
+++ Login-and-SignUp/index.js
@@ -0,0 +1,8 @@
+let allDiv=document.querySelector(".all-div")
+tapBtn=document.querySelector(".tap")
+logIn=document.querySelector(".login-part")
+signUp=document.querySelector(".signUp-part")
+
+function change(){
+    allDiv.classList.toggle("active")
+}
